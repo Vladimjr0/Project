1f0bf242aca4f6c5e3e2f9fb10778e9df509e2e1
parov <vladimjrshevcov@gmail.com>
2024-02-14 17:52:34 +0300

    Реализована функция удаления товара по его названию.

--- src/main/java/com/project/project/controllers/ProductController.java
+++ src/main/java/com/project/project/controllers/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,41 +18,58 @@ public class ProductController {
 
     private final ProductService productService;
 
-
     @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
     @GetMapping("/addproduct")
-    public String showAddProduct(Model model){
+    public String showAddProduct(Model model) {
         model.addAttribute("product", new Product());
         return "addproduct";
     }
+
     @PostMapping("/addproduct")
-    public String addNewProduct(@ModelAttribute Product product){
-        if(productService.addProduct(product)){
+    public String addNewProduct(@ModelAttribute Product product) {
+        if (productService.addProduct(product)) {
             return "redirect:/success";
-        }else {
+        } else {
             return "redirect:/notSuccess";
         }
     }
+
     @GetMapping("/products")
-    public String getAllProducts(Model model){
+    public String getAllProducts(Model model) {
         List<Product> products = productService.getAllProducts();
         model.addAttribute("products", products);
         return "products";
     }
 
-    @GetMapping("/success")
-    public String showSuccess(){
-        return "success";
-    }
-    @GetMapping("/notSuccess")
-    public String showNotSuccess(){
-        return ("notSuccess");
+    @GetMapping("/removeproduct")
+    public String removeProduct(Model model) {
+        model.addAttribute("itemName", "");
+        return "removeproduct";
     }
 
+    @PostMapping("/removeproduct")
+    public String removeProduct(@RequestParam("itemName") String itemName) {
+        if (productService.removeProduct(itemName)) {
+            return "redirect:/success";
+        } else {
+            return "redirect:/notSuccess";
+        }
+    }
+
+
+    @GetMapping("/success")
+    public String showSuccess() {
+        return "success";
+    }
+
+    @GetMapping("/notSuccess")
+    public String showNotSuccess() {
+        return ("notSuccess");
+    }
 
 
 }
