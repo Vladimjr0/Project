2cc9b2a01c7f4866255a216a9165ac1edc107b81
parov <vladimjrshevcov@gmail.com>
2024-02-17 17:58:16 +0300

    Ничего не работает

---
+++ src/main/java/com/project/project/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.project.project.repositories;
+
+import com.project.project.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByUserName(String userName);
+
+}
